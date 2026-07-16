c8c461e50428a04787317430b4f83951121f1e32
FlyQingfeng <2758534648@qq.com>
2026-07-16 15:41:19 +0800

    feat(net): add basic Socket class skeleton and implementation file
    新建了Socket.h头文件和Socket.cpp实现文件，搭建了Socket类的基础框架，用于后续网络套接字相关功能开发

---
+++ muduox/net/Socket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2026/7/15.
+//
+
+#include "Socket.h"
---
+++ muduox/net/Socket.h
@@ -0,0 +1,16 @@
+//
+// Created by Administrator on 2026/7/15.
+//
+
+#ifndef MUDUO_CROSS_SOCKET_H
+#define MUDUO_CROSS_SOCKET_H
+
+
+
+class Socket {
+
+};
+
+
+
+#endif //MUDUO_CROSS_SOCKET_H
